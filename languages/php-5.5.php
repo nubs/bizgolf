0d82743394fd7080917457e933a6b71df2d90ec9
Spencer Rinehart <spencer.rinehart@dominionenterprises.com>
2013-09-24 14:24:28 -0400

    Allow for disabling regex's.

--- languages/php-5.5.php
+++ languages/php-5.5.php
@@ -24,6 +24,23 @@ EOD;
                 case 'character-counting':
                     $disableFunctions = ['count_chars', 'array_count_values'];
                     break;
+                case 'regular-expressions':
+                    $disableFunctions = [
+                        'preg_filter',
+                        'preg_grep',
+                        'preg_match_all',
+                        'preg_match',
+                        'preg_replace_callback',
+                        'preg_replace',
+                        'preg_split',
+                        'ereg',
+                        'ereg_replace',
+                        'eregi',
+                        'eregi_replace',
+                        'split',
+                        'spliti',
+                    ];
+                    break;
             }
 
             if (empty($disableFunctions)) {
